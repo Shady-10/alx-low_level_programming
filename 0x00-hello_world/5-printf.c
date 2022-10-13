c16e956d5296bf4564cceeedca741848ef5f793d
Shady-10 <shadyosama554@gmail.com>
2022-10-13 14:22:23 -0700

    fgdgfd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,37 @@
+/*
+ *
+ *  * File: 4-puts.c
+ *
+ *   * Auth: Shady
+ *
+ *    */
+
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints "with proper grammar, but the outcome is
+ *
+ *   *                a piece of art," followed by a new line.
+ *
+ *    *
+ *
+ *     * Return: Always 0.
+ *
+ *      */
+
+int main(void)
+
+{
+
+		printf("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
+}
