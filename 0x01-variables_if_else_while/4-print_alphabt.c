b0aa5f9bc60bb31c9195e86309ff11cfea0d7a3e
Shady-10 <113646884+Shady-10@users.noreply.github.com>
2022-10-15 01:44:11 +0200

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - rints the alphabet in lower case
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+			continue;
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
