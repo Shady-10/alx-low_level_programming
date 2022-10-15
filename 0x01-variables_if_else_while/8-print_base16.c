bb6b7be1ae94e93f1411bc963e05a9ff6a0ee4ac
Shady-10 <113646884+Shady-10@users.noreply.github.com>
2022-10-15 02:21:38 +0200

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - rints the alphabet in lower case
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 'f'; i++)
+	{
+		if(i==58)
+			i==97;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
