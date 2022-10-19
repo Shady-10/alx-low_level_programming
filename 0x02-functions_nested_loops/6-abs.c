a845a038e3fc876f9ff1af24d1db6ac464232407
Shady-10 <113646884+Shady-10@users.noreply.github.com>
2022-10-20 00:21:40 +0200

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - a function that checks for an alpha character
+ * @c: single letter input
+ * Return: 1 if int c is alpha, 0 if otherwise
+ */
+int _abs(int i)
+{
+	return (abs(i));
+}
