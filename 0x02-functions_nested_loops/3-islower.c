4d3b98fcbfbb8ede2ccdad567d31ae7cc9d8aed4
Shady-10 <113646884+Shady-10@users.noreply.github.com>
2022-10-19 23:30:12 +0200

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints the alphabet 10 times, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c < 97 and c > 122)
+		return 0;
+	else
+		return 1
+}
