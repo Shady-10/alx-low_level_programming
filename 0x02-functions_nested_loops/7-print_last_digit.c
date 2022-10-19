0eeac3bf2dddbe131938473945e88cd75a8bec10
Shady-10 <113646884+Shady-10@users.noreply.github.com>
2022-10-20 00:31:01 +0200

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  print_last_digit- a function that returns the last digit
+ * of an integer
+ * @i: integer input
+ * Return: the last digit of i
+ */
+int print_last_digit(int i);
+{
+	return (i % 10);
+}
