f0b24ae854017566d8ca1a5deda4158b157f0925
Shady-10 <113646884+Shady-10@users.noreply.github.com>
2022-10-21 23:47:03 +0200

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *mul - check a char
+ *@i: variable
+ *
+ *Return: 0 or 2
+ */
+void print_numbers(void);
+{
+	char i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
