2fabc6feb590bbb5c00a9b052ba38d0a08dd80de
Shady-10 <113646884+Shady-10@users.noreply.github.com>
2022-10-21 23:59:39 +0200

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - check a char
+ *
+ *Return: 0 or 2
+ */
+void more_numbers(void)
+{
+	int b;
+	char a;
+
+	for (b = 0; b < 14; b++)
+	{
+		for (a = '0'; a <= '9'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
