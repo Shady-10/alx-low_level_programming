473d6b4799d65678e08549bf12c7e08404686416
Shady-10 <113646884+Shady-10@users.noreply.github.com>
2022-10-22 00:02:58 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,34 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * more_numbers - check a char
- *
- *Return: 0 or 2
- */
+* more_numbers - check a char
+*
+*Return: 0 or 2
+*/
 void more_numbers(void)
 {
-	int b;
-	char a;
+	char a = '0';
+	char b = '0';
+	int i = 0;
 
-	for (b = 0; b < 14; b++)
+	for (; i < 10; i++)
 	{
-		for (a = '0'; a <= '9'; a++)
+		a = '0';
+		b = '0';
+		for (; a <= '1'; a++)
 		{
-			_putchar(a);
+			for (b = '0'; b <= '9'; b++)
+			{
+				if (a != '0')
+					_putchar(a);
+				_putchar(b);
+				if (a == '1' && b == '4')
+				{
+					_putchar('\n');
+					break;
+				}
+			}
 		}
-		_putchar('\n');
 	}
-	_putchar('\n');
 }
