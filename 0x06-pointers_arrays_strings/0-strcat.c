f1f8280a834b6138cb425172af380e271392cc55
Shady-10 <113646884+Shady-10@users.noreply.github.com>
2022-10-29 03:24:34 +0200

    Add files via upload

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*_strcat - concatnate two strings
+*@dest: 1st string
+*@src: 2nd string
+*Return: value
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len1 = 0, len2 = 0;
+
+	while (dest[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (src[len2] != '\0')
+	{
+		dest[len1++] = src[len2++];
+	}
+
+	dest[len1] = '\0';
+
+	return (dest);
+}
